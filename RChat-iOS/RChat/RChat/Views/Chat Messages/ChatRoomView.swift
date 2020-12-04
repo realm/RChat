f1b6732237fb9d3ed322b95ecce2a3ceee3d531c
am-MongoDB <andrew.morgan@mongodb.com>
2020-12-04 13:01:54 +0000

    wip Realm app

---
+++ RChat-iOS/RChat/RChat/Views/Chat Messages/ChatRoomView.swift
@@ -0,0 +1,20 @@
+//
+//  ChatRoomView.swift
+//  RChat
+//
+//  Created by Andrew Morgan on 03/12/2020.
+//
+
+import SwiftUI
+
+struct ChatRoomView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ChatRoomView_Previews: PreviewProvider {
+    static var previews: some View {
+        ChatRoomView()
+    }
+}
