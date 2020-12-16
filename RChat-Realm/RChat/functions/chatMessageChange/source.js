b9836b3e4ac9b3d111ffa49de0c557e6b054e4b9
am-MongoDB <andrew.morgan@mongodb.com>
2020-12-16 10:20:09 +0000

    Update to Realm functions

--- RChat-Realm/RChat/functions/chatMessageChange/source.js
+++ RChat-Realm/RChat/functions/chatMessageChange/source.js
@@ -13,7 +13,7 @@ exports = function(changeEvent) {
     const splitPartition = chatMessage.partition.split("=");
     if (splitPartition.length == 2) {
       conversation = splitPartition[1];
-      console.log(`Partition/converstion = ${conversation}`);
+      console.log(`Partition/conversation = ${conversation}`);
     } else {
       console.log("Couldn't extract the conversation from partition ${chatMessage.partition}");
       return;
@@ -51,44 +51,4 @@ exports = function(changeEvent) {
   }, error => {
     console.log(`Failed to match and update User docs: ${error}`);
   });
-
-  /*
-    A Database Trigger will always call a function with a changeEvent.
-    Documentation on ChangeEvents: https://docs.mongodb.com/manual/reference/change-events/
-
-    Access the _id of the changed document:
-    const docId = changeEvent.documentKey._id;
-
-    Access the latest version of the changed document
-    (with Full Document enabled for Insert, Update, and Replace operations):
-    const fullDocument = changeEvent.fullDocument;
-
-    const updateDescription = changeEvent.updateDescription;
-
-    See which fields were changed (if any):
-    if (updateDescription) {
-      const updatedFields = updateDescription.updatedFields; // A document containing updated fields
-    }
-
-    See which fields were removed (if any):
-    if (updateDescription) {
-      const removedFields = updateDescription.removedFields; // An array of removed fields
-    }
-
-    Functions run by Triggers are run as System users and have full access to Services, Functions, and MongoDB Data.
-
-    Access a mongodb service:
-    const collection = context.services.get("mongodb-atlas").db("RChat").collection("ChatMessage");
-    const doc = collection.findOne({ name: "mongodb" });
-
-    Note: In Atlas Triggers, the service name is defaulted to the cluster name.
-
-    Call other named functions if they are defined in your application:
-    const result = context.functions.execute("function_name", arg1, arg2);
-
-    Access the default http client and execute a GET request:
-    const response = context.http.get({ url: <URL> })
-
-    Learn more about http client here: https://docs.mongodb.com/realm/functions/context/#context-http
-  */
 };
