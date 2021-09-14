cb38bf8dd33b1f87cc9e365c089cc9432bfac0fe
am-MongoDB <andrew.morgan@mongodb.com>
2021-09-14 15:34:32 +0100

    Use new SwiftUI date formatting feature

---
+++ RChat-iOS/RChat/Views/Components/TextDate.swift
@@ -0,0 +1,20 @@
+//
+//  TextDate.swift
+//  TextDate
+//
+//  Created by Andrew Morgan on 14/09/2021.
+//
+
+import SwiftUI
+
+struct TextDate: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TextDate_Previews: PreviewProvider {
+    static var previews: some View {
+        TextDate()
+    }
+}
