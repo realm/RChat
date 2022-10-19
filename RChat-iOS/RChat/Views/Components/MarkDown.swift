231c9076dc89d49fa961e0fe717ccdfc6d2fa7e7
am-MongoDB <andrew.morgan@mongodb.com>
2022-10-19 22:06:20 +0100

    Add back support for markdown

--- RChat-iOS/RChat/Views/Components/MarkDown.swift
+++ RChat-iOS/RChat/Views/Components/MarkDown.swift
@@ -11,12 +11,21 @@ struct MarkDown: View {
     let text: String
 
     var body: some View {
-        Text(try! AttributedString(markdown: text))
+        Text(safeAttributedString(text))
+    }
+}
+
+private func safeAttributedString(_ sourceString: String) -> AttributedString {
+    do {
+        return try AttributedString(markdown: sourceString)
+    } catch {
+        print("Failed to convert Markdown to AttributedString: \(error.localizedDescription)")
+        return try! AttributedString(markdown: "Text could not be rendered")
     }
 }
 
 struct MarkDown_Previews: PreviewProvider {
     static var previews: some View {
-        MarkDown(text: "Sample of *italics*, **bold**, ~~strikethrough~~, [link](https://realm.io) 😀 😃 😄 😁 😆")
+        MarkDown(text: "Some **bold** text")
     }
 }
